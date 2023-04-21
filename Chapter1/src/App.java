e3eebdfbab0bf0cd7509f0b859684215e4074c62
Muhammad Sami <muhammadsami290@outlook.com>
2023-04-21 19:49:06 +0500

    Reinforcement R-1.8

--- Chapter1/src/App.java
+++ Chapter1/src/App.java
@@ -7,7 +7,11 @@ public class App {
         // inputAllBaseTypes();
         // gameEntry();
         // System.out.println(isMultiple(15, 3));
-        System.out.println(-18 << 1);
+        // System.out.println(isEven(1));
+        // System.out.println(sumOfPositiveIntegers(4));
+        // System.out.println(sumOfOddPositiveIntegers(5));
+        // System.out.println(sumOfSquaresOfPositiveIntegers(5));
+        System.out.println(countVowels("Heello"));
     }
 
     // Reinforcement R-1.1
@@ -41,17 +45,17 @@ public class App {
     // Reinforcement R-1.2
     static void gameEntry() {
         class GameEntry {
-            public int score;
+            int score;
 
-            public GameEntry(int score) {
+            GameEntry(int score) {
                 this.score = score;
             }
         }
         GameEntry[] A = { new GameEntry(0), new GameEntry(1), new GameEntry(2), new GameEntry(3) };
-        GameEntry[] B = A.clone();
+        // GameEntry[] B = A.clone();
 
-        // Since array A contains references to object of GameEntry any modification
-        // will reflect also in array B.
+        // Since array A contains references to object of GameEntry any modification to
+        // it will reflect also in array B.
         A[3].score = 550;
     }
 
@@ -62,8 +66,55 @@ public class App {
 
     // Reinforcement R-1.4
     static boolean isEven(int i) {
-        if (i == 2)
-            return true;
-        return i >> 1 > 0 ? true : false;
+        return (i >> 1) << 1 == i ? true : false;
     }
+
+    // Reinforcement R-1.5
+    static int sumOfPositiveIntegers(int n) {
+        if (n < 0)
+            return -1;
+        return n = (n * (n + 1)) / 2;
+        // Another possible solution.
+        // return (int) (Math.pow(n, 2) + n) / 2;
+    }
+
+    // Reinforcement R-1.6
+    static int sumOfOddPositiveIntegers(int n) {
+        if (n < 0)
+            return -1;
+        int sum = 0;
+        while (n != 0) {
+            if (n % 2 != 0)
+                sum += n;
+            n -= 1;
+        }
+        return sum;
+    }
+
+    // Reinforcement R-1.7
+    static int sumOfSquaresOfPositiveIntegers(int n) {
+        if (n < 0)
+            return -1;
+        int sum = 0;
+        while (n != 0) {
+            sum += Math.pow(n, 2);
+            n -= 1;
+        }
+        return sum;
+    }
+
+    // Reinforcement R-1.8
+    static int countVowels(String word) {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < vowels.length; j++) {
+                if (word.charAt(i) == vowels[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
